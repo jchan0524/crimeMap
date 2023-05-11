abbe6737f524b1cbdcce85e49531794e93f27fc3
jchan0524 <jchan@udel.edu>
2023-05-11 06:11:29 -0400

    Added navbar, colored routes based on score,

--- src/components/DirectionMap.js
+++ src/components/DirectionMap.js
@@ -3,6 +3,15 @@
 // Import React and hooks from the react package
 import React, { useEffect } from "react";
 import { useState, useCallback } from "react";
+import {
+  Navbar,
+  Button,
+  FormControl,
+  Form,
+  Col,
+  Row,
+  Container,
+} from "react-bootstrap";
 
 import Geocode from "react-geocode";
 import OpenCageGeocode from "opencage-api-client";
@@ -35,16 +44,13 @@ export default function DirectionMap() {
   const [destinationInput, SetDestinationInput] = useState("");
   const [origin, setOrigin] = useState(null);
   const [buttonPushed, setButtonPushed] = useState(false);
-  const [start, setStart] = useState([]);
-  const [steps, setSteps] = useState();
-
-  const [lat, setLat] = useState();
-  const [long, setLong] = useState();
-  const [lat2, setLat2] = useState();
-  const [long2, setLong2] = useState();
 
   const [count, setCount] = useState(0);
   const [count2, setCount2] = useState(0);
+  const [count3, setCount3] = useState(0);
+
+  const [score, setScore] = useState(0);
+  const [score2, setScore2] = useState(0);
 
   const [token, setToken] = useState(
     "IVBDcEovRGtzL0RhOUdrUFdjaDZuQ0E9PT9nQVdWRVFBQUFBQUFBQUNNQjNWelpYSmZhV1NVakFFeGxJYVVMZz09"
@@ -96,73 +102,124 @@ export default function DirectionMap() {
     console.log(i);
 
     const stepsArray = [];
-
-    i.routes[0].legs.forEach((leg) => {
-      leg.steps.forEach((step) => {
-        const polyline = step.path;
-        polyline.forEach((point) => {
-          stepsArray.push({ lat: point.lat(), lng: point.lng() });
+    if (i.routes[0]) {
+      i.routes[0].legs.forEach((leg) => {
+        leg.steps.forEach((step) => {
+          const polyline = step.path;
+          polyline.forEach((point) => {
+            stepsArray.push({ lat: point.lat(), lng: point.lng() });
+          });
         });
       });
-    });
+    }
     const stepsArray_2 = [];
-
-    i.routes[1].legs.forEach((leg) => {
-      leg.steps.forEach((step) => {
-        const polyline = step.path;
-        polyline.forEach((point) => {
-          stepsArray_2.push({ lat: point.lat(), lng: point.lng() });
+    if (i.routes[1]) {
+      i.routes[1].legs.forEach((leg) => {
+        leg.steps.forEach((step) => {
+          const polyline = step.path;
+          polyline.forEach((point) => {
+            stepsArray_2.push({ lat: point.lat(), lng: point.lng() });
+          });
         });
       });
+    }
+
+    const stepsArray_3 = [];
+
+    if (i.routes[2]) {
+      i.routes[2].legs.forEach((leg) => {
+        leg.steps.forEach((step) => {
+          const polyline = step.path;
+          polyline.forEach((point) => {
+            stepsArray.push({ lat: point.lat(), lng: point.lng() });
+          });
+        });
+      });
+    }
+
+    const fetches = stepsArray.map((step) => {
+      return fetch(
+        `https://api.crimemap.hopto.org/get/incidents?filter=(latitude >= ${
+          step.lat
+        } AND latitude <= ${step.lat + 0.0002} AND longitude >= ${
+          step.lng
+        } AND longitude <= ${step.lng + 0.0002})&token=${token}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          return data;
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
     });
-    // const myArray = ['apple', 'banana', 'cherry'];
-    // for (let b = 0; b < myArray.length; b++) {
-    //   window["item_" + b] = i[b];
-    //   console.log(item_0)
-    // }
-    
 
-    for (let i = 0; i < stepsArray.length; i++) {
-      fetch(
-        `https://api.crimemap.hopto.org/get/incidents?filter=(latitude >= ${
-          stepsArray[i].lat
-        } AND latitude <= ${stepsArray[i].lat + 0.0002} AND longitude >= ${
-          stepsArray[i].lng
-        } AND longitude <= ${stepsArray[i].lng + 0.0002})&token=${token}`
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
+    Promise.all(fetches)
+      .then((dataArr) => {
+        dataArr.forEach((data) => {
           calculateScore(data);
-        })
-        .catch((error) => {
-          console.error("Error:", error);
         });
-    }
+      })
+      .catch((error) => console.error("Error:", error));
 
-    for (let i = 0; i < stepsArray_2.length; i++) {
-      fetch(
+    const fetches_2 = stepsArray_2.map((step) => {
+      return fetch(
         `https://api.crimemap.hopto.org/get/incidents?filter=(latitude >= ${
-          stepsArray_2[i].lat
-        } AND latitude <= ${stepsArray_2[i].lat + 0.0002} AND longitude >= ${
-          stepsArray_2[i].lng
-        } AND longitude <= ${stepsArray_2[i].lng + 0.0002})&token=${token}`
+          step.lat
+        } AND latitude <= ${step.lat + 0.0002} AND longitude >= ${
+          step.lng
+        } AND longitude <= ${step.lng + 0.0002})&token=${token}`
       )
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
-          calculateScore2(data);
+          return data;
         })
         .catch((error) => {
           console.error("Error:", error);
         });
-    }
+    });
+
+    Promise.all(fetches_2)
+      .then((dataArr) => {
+        dataArr.forEach((data) => {
+          calculateScore2(data);
+        });
+      })
+      .catch((error) => console.error("Error:", error));
+
+    const fetches_3 = stepsArray.map((step) => {
+      return fetch(
+        `https://api.crimemap.hopto.org/get/incidents?filter=(latitude >= ${
+          step.lat
+        } AND latitude <= ${step.lat + 0.0002} AND longitude >= ${
+          step.lng
+        } AND longitude <= ${step.lng + 0.0002})&token=${token}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          return data;
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
+
+    Promise.all(fetches_3)
+      .then((dataArr) => {
+        dataArr.forEach((data) => {
+          calculateScore3(data);
+        });
+      })
+      .catch((error) => console.error("Error:", error));
   };
 
   const calculateScore = useCallback((response) => {
     setCount((prevCount) => {
       let tmpCount = prevCount;
-     
+
       if (!response.data.submitted) {
         if (Array.isArray(response.data)) {
           for (let j = 0; j < response.data.length; j++) {
@@ -172,7 +229,8 @@ export default function DirectionMap() {
           tmpCount += response.data.tier * 10;
         }
       }
-     
+      setScore(tmpCount);
+
       return tmpCount;
     });
   }, []);
@@ -180,7 +238,7 @@ export default function DirectionMap() {
   const calculateScore2 = useCallback((response) => {
     setCount2((prevCount2) => {
       let tmpCount2 = prevCount2;
-   
+
       if (!response.data.submitted) {
         if (Array.isArray(response.data)) {
           for (let j = 0; j < response.data.length; j++) {
@@ -190,7 +248,25 @@ export default function DirectionMap() {
           tmpCount2 += response.data.tier * 10;
         }
       }
-  
+
+      return tmpCount2;
+    });
+  }, []);
+
+  const calculateScore3 = useCallback((response) => {
+    setCount3((prevCount2) => {
+      let tmpCount2 = prevCount2;
+
+      if (!response.data.submitted) {
+        if (Array.isArray(response.data)) {
+          for (let j = 0; j < response.data.length; j++) {
+            tmpCount2 += response.data[j].tier * 10;
+          }
+        } else {
+          tmpCount2 += response.data.tier * 10;
+        }
+      }
+
       return tmpCount2;
     });
   }, []);
@@ -201,77 +277,181 @@ export default function DirectionMap() {
     console.log("start 2", count2);
   }, [count, count2]);
 
+  function getColorsFromColorCounts(
+    route1CrimeCount,
+    route2CrimeCount,
+    route3CrimeCount
+  ) {
+    const colors = ["green", "yellow", "red"];
+    const colorAssignments = Array(3).fill(null);
+
+    if (
+      route1CrimeCount <= route2CrimeCount &&
+      route1CrimeCount <= route3CrimeCount
+    ) {
+      colorAssignments[0] = colors[0];
+      if (route2CrimeCount <= route3CrimeCount) {
+        colorAssignments[1] = colors[1];
+        colorAssignments[2] = colors[2];
+      } else {
+        colorAssignments[1] = colors[2];
+        colorAssignments[2] = colors[1];
+      }
+    } else if (
+      route2CrimeCount <= route1CrimeCount &&
+      route2CrimeCount <= route3CrimeCount
+    ) {
+      colorAssignments[0] = colors[1];
+      colorAssignments[1] = colors[0];
+      if (route1CrimeCount <= route3CrimeCount) {
+        colorAssignments[2] = colors[2];
+      } else {
+        colorAssignments[2] = colors[1];
+      }
+    } else {
+      colorAssignments[0] = colors[2];
+      if (route1CrimeCount <= route2CrimeCount) {
+        colorAssignments[1] = colors[1];
+        colorAssignments[2] = colors[0];
+      } else {
+        colorAssignments[1] = colors[0];
+        colorAssignments[2] = colors[1];
+      }
+    }
+
+    return colorAssignments;
+  }
+
+  const colorAssignments = getColorsFromColorCounts(count, count2, count3);
+
   // Render the component
   return (
     // Load the Google Maps API with the provided API key
     <LoadScript googleMapsApiKey="AIzaSyCB4V63QJ8uz0HDmMrr_KUkuz85VcbZEZU">
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/">Direction Map</Navbar.Brand>
+      </Navbar>
       <div>
         {/* Render a form to input origin and destination locations */}
-        <form onSubmit={handleSubmit}>
-          <label> Origin: </label>
-          <input
+        <Form inline onSubmit={handleSubmit}>
+          <FormControl
             type="text"
+            placeholder="Origin"
+            className="mr-sm-2"
             value={originInput}
             onChange={(e) => setOriginInput(e.target.value)}
           />
-
-          <label>Destination:</label>
-          <input
+          <FormControl
             type="text"
+            placeholder="Destination"
+            className="mr-sm-2"
             value={destinationInput}
             onChange={(e) => SetDestinationInput(e.target.value)}
           />
-
-          <button type="submit"> Get Directions</button>
-        </form>
+          <Button type="submit">Get Directions</Button>
+        </Form>
 
         {loading ? (
           <div>Loading</div>
         ) : (
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={12}
-          >
-            {origin && destination && buttonPushed && (
-              <DirectionsService
-                options={{
-                  destination: destination,
-                  origin: origin,
-                  travelMode: "WALKING",
-                  provideRouteAlternatives: true,
-                }}
-                callback={(response) => {
-                  if (response !== null) {
-                    setDirections(response);
-                    
-                  
-                    calculateSteps(response);
+          <div>
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={12}
+            >
+              {origin && destination && buttonPushed && (
+                <DirectionsService
+                  options={{
+                    destination: destination,
+                    origin: origin,
+                    travelMode: "WALKING",
+                    provideRouteAlternatives: true,
+                  }}
+                  callback={(response) => {
+                    if (response !== null) {
+                      setCount(0);
+                      setCount2(0);
+                      setCount3(0);
+                      setDirections(response);
 
-                    setButtonPushed(false);
-                  }
-                }}
-              />
-            )}
+                      calculateSteps(response);
 
-            {directions && (
-              <>
-                {/* <DirectionsRenderer
-                  options={{ directions: directions, routeIndex: selected }}
-                /> */}
-                <DirectionsRenderer
-                  options={{ directions: directions, routeIndex: 1 }}
+                      setButtonPushed(false);
+                    }
+                  }}
                 />
-                <DirectionsRenderer
-                  options={{ directions: directions, routeIndex: 2 }}
-                />
-                <DirectionsRenderer
-                  options={{ directions: directions, routeIndex: 0 }}
-                />
-              </>
-            )}
-          </GoogleMap>
+              )}
+
+              {directions && (
+                <>
+                  <DirectionsRenderer
+                    options={{
+                      directions: directions,
+                      routeIndex: 0,
+                      polylineOptions: {
+                        strokeColor: colorAssignments[0],
+                      },
+                    }}
+                  />
+                  {directions.routes[1] && (
+                    <DirectionsRenderer
+                      options={{
+                        directions: directions,
+                        routeIndex: 1,
+                        polylineOptions: {
+                          strokeColor: colorAssignments[1],
+                        },
+                      }}
+                    />
+                  )}
+                  {directions.routes[2] && (
+                    <DirectionsRenderer
+                      options={{
+                        directions: directions,
+                        routeIndex: 2,
+                        polylineOptions: {
+                          strokeColor: colorAssignments[2],
+                        },
+                      }}
+                    />
+                  )}
+                </>
+              )}
+            </GoogleMap>
+          </div>
         )}
+        <Container>
+          <Form>
+            <Row className="mb-3">
+              <Col>
+                <Form.Control
+                  placeholder="Route 1 Score: "
+                  value={count}
+                  readOnly
+                />
+              </Col>
+              {directions && directions.routes[1] && (
+                <Col>
+                  <Form.Control
+                    placeholder="Route 2 Score: "
+                    value={count2}
+                    readOnly
+                  />
+                </Col>
+              )}
+              {directions && directions.routes[2] && (
+                <Col>
+                  <Form.Control
+                    placeholder="Route 3 Score: "
+                    value={count3}
+                    readOnly
+                  />
+                </Col>
+              )}
+            </Row>
+          </Form>
+        </Container>
       </div>
     </LoadScript>
   );
